ae5ba6d3e879f6cb232178e9f81abae1eb7c13bc
Chet Gray <chet.gray@waystar.com>
2022-10-18 13:49:56 -0400

    ✨ feat: introduce Address BLL, DTO, and Repository

---
+++ DataLibrary/Repositories/AddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataLibrary.DTOs;
+
+namespace DataLibrary.Repositories
+{
+    public class AddressRepository
+    {
+        public AddressDTO GetAddressById(int addressId)
+        {
+            AddressDTO address = new AddressDTO() { Id = addressId };
+
+            return address;
+        }
+    }
+}
---
+++ EmployeeApp/Business/AddressBLL.cs
@@ -0,0 +1,25 @@
+﻿using DataLibrary.DTOs;
+using DataLibrary.Repositories;
+
+using EmployeeApp.Models;
+
+namespace EmployeeApp.Business
+{
+    internal class AddressBLL
+    {
+        private readonly AddressRepository _addressRepository = new AddressRepository();
+
+        public Address GetAddressById(int addressId)
+        {
+            AddressDTO dto = _addressRepository.GetAddressById(addressId);
+            Address address = new Address()
+            {
+                StreetAddress = dto.StreetAddress,
+                City = dto.City,
+                State = dto.State,
+                Zip = dto.Zip
+            };
+            return address;
+        }
+    }
+}
