573c793514ff4fda5007767caba271833f85e6ee
Chet Gray <chet.gray@waystar.com>
2022-10-18 17:51:00 -0400

    ✨ feat: introduce DAL and `GetRecordsViaStoredProcedure` method
    > - The app will use the Employee database we created earlier

---
+++ DataLibrary/DAL/EmployeeAppDAL.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLibrary.DAL
+{
+    internal class EmployeeAppDAL : IEmployeeAppDAL
+    {
+        private string _connectionString;
+
+        public EmployeeAppDAL(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<object[]> GetRecordsViaStoredProcedure(
+            string storedProcedure,
+            Dictionary<string, object> parameters
+        )
+        {
+            List<object[]> records = new List<object[]>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(storedProcedure, connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                foreach (var parameter in parameters)
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            object[] record = new object[reader.FieldCount];
+                            reader.GetValues(record);
+                            records.Add(record);
+                        }
+                    }
+                }
+            }
+
+            return records;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 > Create a console app with a DAL, using the Repository pattern.
 > 
-> - [ ] The app will use the Employee database we created earlier
+> - [x] The app will use the Employee database we created earlier
 > - [x] The app should allow the user to choose from a menu of options
 >     - [ ] Option 1:  See a list of all employees and their Addresses and
 >       Start Dates
 >     - [ ] Option 2:  See all employees who live in a certain state
 >     - [ ] Option 3:  See all employees who started after a certain date
-> - [ ] Be sure to use Business Logic, Models, Repositories, a DAL, an
+> - [x] Be sure to use Business Logic, Models, Repositories, a DAL, an
 >   Interface for your DAL, DTOs, and Stored Procedures
