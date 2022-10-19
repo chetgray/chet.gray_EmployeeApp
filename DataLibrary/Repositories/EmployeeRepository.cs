c05d2a49f63dc0bccfd819976df21375132758ee
Chet Gray <chet.gray@waystar.com>
2022-10-19 11:38:41 -0400

    ✨ feat: implement GetAll for Employee

--- DataLibrary/Repositories/EmployeeRepository.cs
+++ DataLibrary/Repositories/EmployeeRepository.cs
@@ -41,9 +41,30 @@ namespace DataLibrary.Repositories
         public List<EmployeeDTO> GetAll()
         {
             List<EmployeeDTO> employees = new List<EmployeeDTO>();
+            EmployeeAppDAL dal = new EmployeeAppDAL(
+                ConfigurationManager.ConnectionStrings["EmployeeDatabase"].ConnectionString
+            );
+            List<object[]> records = dal.GetRecordsViaStoredProcedure(
+                "spEmployeeGetAll",
+                new Dictionary<string, object>()
+            );
 
-            employees.Add(GetById(1));
-            employees[0].MiddleName = "\"I AM EVERYONE\"";
+            foreach (object[] record in records)
+            {
+                employees.Add(
+                    new EmployeeDTO
+                    {
+                        Id = (int)record[0],
+                        FirstName = (string)record[1],
+                        MiddleName = (string)record[2],
+                        LastName = (string)record[3],
+                        DateOfBirth = DateTime.Parse((string)record[4]),
+                        EmploymentStartDate = DateTime.Parse((string)record[5]),
+                        EmploymentEndDate = DateTime.Parse((string)record[6]),
+                        AddressId = (int?)record[7]
+                    }
+                );
+            }
 
             return employees;
         }
---
+++ Employee.Database/dbo/Stored Procedures/spEmployeeGetAll.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE dbo.spEmployeeGetAll
+AS
+SELECT
+    e.Id
+    , e.FirstName
+    , e.MiddleName
+    , e.LastName
+    , e.[Date of Birth]
+    , e.[Employment Start Date]
+    , e.[Employment End Date]
+    , e.Address_ID
+FROM
+    dbo.GeneralInfo AS e
