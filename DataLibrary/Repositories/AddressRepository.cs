a802e946b74b8bd5895e83f987d65dd9722f9033
Chet Gray <chet.gray@waystar.com>
2022-10-19 10:34:54 -0400

    ✨ feat: introduce AddressGetById stored procedure and use it in repository method

--- DataLibrary/Repositories/AddressRepository.cs
+++ DataLibrary/Repositories/AddressRepository.cs
@@ -1,4 +1,8 @@
-﻿using DataLibrary.DTOs;
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+using DataLibrary.DAL;
+using DataLibrary.DTOs;
 
 namespace DataLibrary.Repositories
 {
@@ -6,7 +10,26 @@ namespace DataLibrary.Repositories
     {
         public AddressDTO GetById(int addressId)
         {
-            AddressDTO address = new AddressDTO() { Id = addressId };
+            IEmployeeAppDAL dal = new EmployeeAppDAL(
+                ConfigurationManager.ConnectionStrings["EmployeeDatabase"].ConnectionString
+            );
+            List<object[]> records = dal.GetRecordsViaStoredProcedure(
+                "dbo.spAddressGetById",
+                new Dictionary<string, object> { { "@addressId", addressId } }
+            );
+
+            if (records.Count == 0)
+            {
+                return null;
+            }
+            AddressDTO address = new AddressDTO
+            {
+                Id = (int)records[0][0],
+                StreetAddress = (string)records[0][1],
+                City = (string)records[0][2],
+                State = (string)records[0][3],
+                Zip = (string)records[0][4]
+            };
 
             return address;
         }
